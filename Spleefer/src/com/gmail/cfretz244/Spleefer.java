98a9d21c2f1ce08aee7ce800cc8eb76301ca4125
Cfretz244 <cfretz@christophersmbp.home>
2014-01-26 01:14:09 -0500

    spleef arena setup complete
    untested

--- Spleefer/src/com/gmail/cfretz244/Spleefer.java
+++ Spleefer/src/com/gmail/cfretz244/Spleefer.java
@@ -4,40 +4,30 @@ import java.util.HashSet;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Spleefer extends JavaPlugin implements Listener {
 	
 	HashSet<String> registeredPlayers;
-	
-	Location[] killRegion;
-	Location[] floorRegion;
-	Location[] fightingRoomRegion;
-	Location[] spectatingRoomRegion;
-	Location[] spawningRegion;
+	Location[] killRegion, floorRegion, fightingRoomRegion, spectatingRoomRegion, spawningRegion;
+	SpleefListener listener;
+	Executor utility;
+	boolean settingUp, definingFightingRoom, definingFloor, definingSpawn, definingKill, definingSpectation, finishedDefinition;
 	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
 		registeredPlayers = new HashSet<String>();
 		killRegion = new Location[2];
 		floorRegion = new Location[2];
 		fightingRoomRegion = new Location[3];
 		spectatingRoomRegion = new Location[3];
 		spawningRegion = new Location[3];
-		
-		getLogger().info("Spleefer Loaded");
+		listener = new SpleefListener(killRegion, floorRegion, fightingRoomRegion, spectatingRoomRegion, spawningRegion, utility, this);
+		utility = new Executor(killRegion, floorRegion, fightingRoomRegion, spectatingRoomRegion, spawningRegion, this);
 	}
 	
 	@Override
@@ -45,159 +35,115 @@ public final class Spleefer extends JavaPlugin implements Listener {
 		
 	}
 	
-	@EventHandler
-	public void onPlayerInteractBlock(PlayerInteractEvent event) {
-		Player sender = event.getPlayer();
-		ItemStack currentItem = sender.getItemInHand();
-		Material itemType = currentItem.getType();
-		if(itemType == Material.BONE) {
-			if(event.getAction() == Action.LEFT_CLICK_BLOCK) {
-				if(registeredPlayers.contains(sender.getName())) {
-					killRegion[0] = sender.getTargetBlock(null, 20).getLocation();
-					sender.sendMessage(ChatColor.LIGHT_PURPLE + "First Position Set at " + killRegion[0].getBlockX() + ", " + killRegion[0].getBlockY() + ", " + killRegion[0].getBlockZ());
-				}
-			} else if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-				if(registeredPlayers.contains(sender.getName())) {
-					killRegion[1] = sender.getTargetBlock(null, 20).getLocation();
-					sender.sendMessage(ChatColor.LIGHT_PURPLE + "Second Position Set at " + killRegion[1].getBlockX() + ", " + killRegion[1].getBlockY() + ", " + killRegion[1].getBlockZ());
-				}
-			}
-		}
-	}
-	
-	public boolean containedIn(Location[] actionSpace, Location playerLoc) {
-		if(actionSpace[0] == null || actionSpace[1] == null || playerLoc == null) {
-			return false;
-		}
-		Location point1 = actionSpace[0];
-		Location point2 = actionSpace[1];
-		double X1 = point1.getX();
-		double X2 = point2.getX();
-		double Z1 = point1.getZ();
-		double Z2 = point2.getZ();
-		double Y1 = point1.getY();
-		double Y2 = point2.getY();
-		double pX = Math.floor(playerLoc.getX());
-		double pY = Math.floor(playerLoc.getY()) - 1;
-		double pZ = Math.floor(playerLoc.getZ());
-		if(Y1 > Y2 && (pY < Y2 || pY > Y1)) {
-			return false;
-		} else if(Y1 == Y2 && pY != Y1) {
-			return false;
-		} else if(Y2 > Y1 && (pY < Y1 || pY > Y2)) {
-			return false;
-		}
-		if(X1 < X2 && Z1 > Z2) {
-			//case 1
-			if(pX >= X1 && pX <= X2 && pZ >= Z2 && pZ <= Z1) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X2 < X1 && Z1 > Z2) {
-			//case 2
-			if(pX >= X2 && pX <= X1 && pZ >= X2 && pZ <= Z1) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X1 < X2 && Z2 > Z1) {
-			//case 3
-			if(pX >= X1 && pX <= X2 && pZ >= Z1 && pZ <= Z2) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X2 < X1 && Z2 > Z1) {
-			//case 4
-			if(pX >= X2 && pX <= X1 && pZ >= Z1 && pZ <= Z2) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X1 < X2 && Z1 == Z2) {
-			//case 5
-			pZ = Math.floor(pZ);
-			if(pX >= X1 && pX <= X2 && pZ == Z1) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X2 < X1 && Z1 == Z2) {
-			//case 6
-			pZ = Math.floor(pZ);
-			if(pX >= X2 && pX <= X1 && pZ == Z1) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X1 == X2 && Z2 > Z1) {
-			//case 7
-			pX = Math.floor(pX);
-			if(pZ >= Z1 && pZ <= Z2 && pX == X1) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if(X1 == X2 && Z1 > Z2) {
-			//case 8
-			pX = Math.floor(pX);
-			if(pZ >= Z2 && pZ >= Z1 && pX == X1) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return false;
-	}
-	
-	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent event) {
-		Player player = event.getPlayer();
-		if(registeredPlayers.contains(player.getName())) {
-			if(containedIn(killRegion, player.getLocation())) {
-				player.sendMessage("You're in the space");
-			} else {
-				player.sendMessage("You're not in the space");
-			}
-		}
-	}
-	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(sender instanceof Player) {
 			Player player = (Player) sender;
 			String name = player.getName();
 			registeredPlayers.add(name);
-			if(cmd.getName().toLowerCase().equals("spleefsetup")) {
-				Inventory playerInv = player.getInventory();
-				boolean needsToBeReplaced = true;
-				int chosenSlot = -1;
-				for(int i = 0; i < 36; i++) {
-					ItemStack stack = playerInv.getItem(i);
-					if(stack == null) {
-						if(chosenSlot == -1) {
-							chosenSlot = i;
+			if(cmd.getName().toLowerCase().equals("spleef")) {
+				if(args[0].equals("setup")) {
+					if(!settingUp) {
+						settingUp = true;
+						definingFightingRoom = true;
+						listener.settingUp = true;
+						utility.giveSpleefWand(player);
+						player.sendMessage(ChatColor.YELLOW + "[Spleefer] Please define the fighting room. Use \"/spleef done\" to indicate you've chosen");
+						getServer().getPluginManager().registerEvents(listener, this);
+						listener.definingFightingRoom = true;
+					} else {
+						player.sendMessage(ChatColor.RED + "[Spleefer] You're already setting up the spleef arena.");
+					}
+					
+				} else if(args[0].equals("done")) {
+					if(settingUp) {
+						if(definingFightingRoom) {
+							definingFightingRoom = false;
+							listener.definingFightingRoom = false;
+							player.sendMessage(ChatColor.YELLOW + "[Spleefer] Please define the spleef arena floor. Use\"/spleef done\" to indicate you've chosen.");
+							definingFloor = true;
+							listener.definingFloor = true;
+						} else if(definingFloor) {
+							definingFloor = false;
+							listener.definingFloor = false;
+							player.sendMessage(ChatColor.YELLOW + "[Spleefer] Please define the spleef spawning region. Use \"/spleef done\" to indicate you've chosen.");
+							definingSpawn = true;
+							listener.definingSpawn = true;
+						} else if(definingSpawn) {
+							definingSpawn = false;
+							listener.definingSpawn = false;
+							player.sendMessage(ChatColor.YELLOW + "[Spleefer] Please define the spleef kill region. Use \"/spleef done\" to indicate you've chosen.");
+							definingKill = true;
+							listener.definingKill = true;
+						} else if(definingKill) {
+							definingKill = false;
+							listener.definingKill = false;
+							player.sendMessage(ChatColor.YELLOW + "[Spleefer] Please define the spleef spectators room. Use \"/spleef done\" to indicate you've chosen.");
+							definingSpectation = true;
+							listener.definingSpectation = true;
+						} else if(definingSpectation) {
+							definingSpectation = false;
+							listener.definingSpectation = false;
+							player.sendMessage(ChatColor.GREEN + "[Spleefer] Finished Definitions. If you're happy with your choices, use \"/spleef save\" to finalize it. Otherwise use \"/spleef discard\".");
+							finishedDefinition = true;
+							listener.finishedDefinition = true;
+						} else {
+							player.sendMessage(ChatColor.RED + "[Spleefer] Really not sure how you got here. This message was added more for completeness than anything else. Either way, something is wrong. I guess try resetting the server?");
 						}
 					} else {
-						Material stacktype = stack.getType();
-						if(stacktype.getId() == Material.BONE.getId()) {
-							needsToBeReplaced = false;
-							chosenSlot = i;
+						player.sendMessage(ChatColor.RED + "[Spleefer] But you weren't doing anything.");
+					}
+				} else if(args[0].equals("save")) {
+					if(settingUp) {
+						if(finishedDefinition) {
+							finishedDefinition = false;
+							//save
+						} else {
+							player.sendMessage(ChatColor.RED + "[Spleefer] Really not sure how you got here. This message was added more for completeness than anything else. Either way, something is wrong. I guess try resetting the server?");
 						}
+					} else {
+						player.sendMessage(ChatColor.RED + "[Spleefer] There's nothing to save.");
+					}
+				} else if(args[0].equals("discard")) {
+					if(settingUp) {
+						for(int i = 0; i < 3; i++) {
+							if(i < 3) {
+								killRegion[i] = null;
+								floorRegion[i] = null;
+							}
+							fightingRoomRegion[i] = null;
+							spectatingRoomRegion[i] = null;
+							spawningRegion[i] = null;
+						}
+						settingUp = false;
+						player.sendMessage(ChatColor.GREEN + "[Spleefer] Done. Better luck next time.");
+					} else {
+						player.sendMessage(ChatColor.RED + "[Spleefer] There's nothing to discard.");
+					}
+				} else if(args[0].equals("begin")) {
+					if(utility.validateSpleefState()) {
+						if(args.length > 1) {
+							if(utility.validatePlayerNames(player, args, getServer())) {
+								for(int i = 0; i < args.length; i++) {
+									registeredPlayers.add(args[i]);
+								}
+								runRound();
+							}
+						} else {
+							player.sendMessage(ChatColor.RED + "[Spleefer] Sorry, you need to list at least 2 players to begin a round.");
+						}
+					} else {
+						player.sendMessage(ChatColor.RED + "[Spleefer] Sorry, you haven't defined the spleef arena yet.");
 					}
 				}
-				if(chosenSlot != -1) {
-					if(needsToBeReplaced) {
-						ItemStack replacementStack = new ItemStack(Material.BONE);
-						playerInv.setItem(chosenSlot, replacementStack);
-					}
-				}
-				
 			}
 		} else {
-			sender.sendMessage("Sender must be a player");
+			sender.sendMessage(ChatColor.RED + "[Spleefer] Sender must be a player");
 		}
 		return false;
+	}
+	
+	public void runRound() {
+		utility.movePlayersToSpawn(getServer());
 	}
 	
 }
